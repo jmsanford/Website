fe6eacced2a9dc260ccb79dc13a1a6e68c359767
jmsanford <jennifermariesanford@gmail.com>
2018-04-11 16:01:34 -0600

    finished basic web design

--- app.js
+++ app.js
@@ -18,10 +18,41 @@ app.get("/projects", function(req,res){
   res.render("projects", {page: 'projects'})
 });
 
-app.get("/blog", function(req,res){
-  res.render("blog", {page: 'blog'})
+app.get("/courses", function(req,res){
+  res.render("courses", {page: 'courses'})
 });
 
+
+
+// TOP KNOT CLIMBING PAGES
+
+app.get("/topknotclimbing/home", function(req,res){
+  res.render("topknotclimbing/topknotclimbing", {page: 'topknotclimbing'})
+});
+
+app.get("/topknotclimbing/about", function(req,res){
+  res.render("topknotclimbing/about", {page: 'about'})
+});
+
+app.get("/topknotclimbing/visit", function(req,res){
+  res.render("topknotclimbing/visit", {page: 'visit'})
+});
+
+app.get("/topknotclimbing/studio", function(req,res){
+  res.render("topknotclimbing/studio", {page: 'studio'})
+});
+
+app.get("/topknotclimbing/boulderingguide", function(req,res){
+  res.render("topknotclimbing/boulderingguide", {page: 'boulderingguide'})
+});
+
+app.get("/topknotclimbing/meetups", function(req,res){
+  res.render("topknotclimbing/meetups", {page: 'meetups'})
+});
+
+app.get("/topknotclimbing/gearshop", function(req,res){
+  res.render("topknotclimbing/gearshop", {page: 'gearshop'})
+});
 
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
     console.log("Server listening...");
